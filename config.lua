e8163f50e18e99f463808647b4aa94675a8eed43
Merodicoexe <hitherl@seznam.cz>
2025-06-14 19:58:39 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,36 @@
+Config = {}
+
+Config.StartCoords = vector3(379.836365, 356.333862, 102.755470) -- NPC pozice
+
+Config.GarageList = vector3(-533.886719, 483.088501, 103.171913) -- označený cíl na mapě
+
+Config.Interior = {
+    enter = vector3(-533.886719, 483.088501, 103.171913),  -- vstup
+    inside = vector3(201.846054, -1007.761536, -98.999969),   -- teleport cíl
+}
+
+Config.KeySearchPoints = {
+    vector3(206.071182, -994.693054, -98.999969),
+    vector3(190.450455, -998.312073, -98.999969),
+    vector3(191.069366, -1005.109680, -98.999992),
+    vector3(206.841583, -1004.232361, -98.999992),
+}
+
+Config.VehicleSpawn = {
+    coords = vector4(195.192078, -1001.841614, -99.411980, 180.762833),
+    model = 'sultan'
+}
+
+Config.ExitVehicle = vector4(-536.883179, 484.592285, 102.964111, 58.212559) -- kam se vozidlo teleportne po výjezdu
+
+Config.Timer = 0 -- v minutách
+Config.Jobs = {'police', 'sheriff'}
+
+Config.DorucitVozidlo = {
+    vector3(-354.946899, 6335.160645, 29.836441),
+    vector3(-3168.950928, 1105.094971, 20.713486)
+}
+
+Config.Castka = '1000'  -- Nastavte pevnou hodnotu
+
+Config.NPCModel = 's_m_m_autoshop_01' -- můžeš změnit
